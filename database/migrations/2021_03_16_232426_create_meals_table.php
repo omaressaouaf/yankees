fcc4af2c9e1b2733924ff85bb24fd838b1e0c434
Omar-Ess <souafomar@gmail.com>
2021-07-05 22:17:29 +0100

    updated meals migration image to nullable

--- database/migrations/2021_03_16_232426_create_meals_table.php
+++ database/migrations/2021_03_16_232426_create_meals_table.php
@@ -17,8 +17,8 @@ class CreateMealsTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('desc')->nullable();
-            $table->text('image')->default('/storage/images/meals/noimage.jpg');
-            $table->text('resizedImage')->default('/storage/images/meals/noimage.jpg');
+            $table->text('image')->default('/storage/images/meals/noimage.jpg')->nullable();
+            $table->text('resizedImage')->default('/storage/images/meals/noimage.jpg')->nullable();
             $table->integer('price');
             $table->boolean('active');
             $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
