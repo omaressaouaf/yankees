d77dd7a2af0caa9e607d0d974d9306e26caf3a9a
Omar-Ess <souafomar@gmail.com>
2021-07-08 12:53:52 +0100

    updated footer copyright

--- resources/js/admin/components/partials/TheFooter.vue
+++ resources/js/admin/components/partials/TheFooter.vue
@@ -1,24 +1,15 @@
 <template>
   <footer class="footer d-print-none">
     <div class="container-fluid">
-      <!-- <nav class="float-left">
-        <ul>
-          <li>
-            <a href="https://www.creative-tim.com"> Creative Tim </a>
-          </li>
-          <li>
-            <a href="https://creative-tim.com/presentation"> About Us </a>
-          </li>
-          <li>
-            <a href="http://blog.creative-tim.com"> Blog </a>
-          </li>
-          <li>
-            <a href="https://www.creative-tim.com/license"> Licenses </a>
-          </li>
-        </ul>
-      </nav> -->
+      <nav class="float-left">
       <div class="copyright float-right">
-        &copy; , made with <i class="material-icons">favorite</i> by
+        &copy; Yankees. All Rights Reserved
+
+      </div>
+      </nav>
+
+      <div class="copyright float-right">
+        &copy; , template made with <i class="material-icons">favorite</i> by
         <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
         for a better web.
       </div>
