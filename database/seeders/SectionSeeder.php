6e5000f132283d48cc541d701555be85480933be
Omar-Ess <souafomar@gmail.com>
2021-07-08 11:39:02 +0100

    removed header seeder

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -15,13 +15,6 @@ class SectionSeeder extends Seeder
     public function run()
     {
         Section::create([
-            'name' => "header",
-            'title_en' => "Welcome to",
-            "title_fr" => "Bienvenue au",
-            "content_en" => "Providing great food for over 18 years!",
-            "content_fr" => "Offrir de la bonne nourriture depuis plus de 18 ans!"
-        ]);
-        Section::create([
             'name' => "about",
             'title_en' => "ENGLISH .Lorem, ipsum dolor sit amet consectetur adipisicing elit. ",
             "title_fr" => "FRENCH .Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
